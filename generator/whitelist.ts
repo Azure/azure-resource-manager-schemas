4c116569e3b7ba135e26d9aa2da2a99e2412329c
Anthony Martin <antmarti@microsoft.com>
2019-10-01 16:29:43 -0700

    Onboard containerservice for autogeneration

--- generator/whitelist.ts
+++ generator/whitelist.ts
@@ -82,6 +82,10 @@ const whitelist: WhitelistConfig[] = [
         basePath: 'relay/resource-manager',
         namespace: 'Microsoft.Relay',
     },
+    { 
+        basePath: 'containerservice/resource-manager',
+        namespace: 'Microsoft.ContainerService',
+    },
 ];
 
 function findWhitelistConfig(basePath: string) {

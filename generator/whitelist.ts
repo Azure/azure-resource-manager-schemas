5d275586b060236d1bd996800d31a26f5fec28b6
hunmix <youarewang@126.com>
2020-03-20 12:17:04 +0800

    regenerate

--- generator/whitelist.ts
+++ generator/whitelist.ts
@@ -137,6 +137,16 @@ const whitelist: WhitelistConfig[] = [
         namespace: 'Microsoft.DeploymentManager',
     },
     {
+      basePath: 'digitaltwins/resource-manager',
+      namespace: 'Microsoft.DigitalTwins',
+      resourceConfig: [
+          {
+              type: 'integrationResources',
+              scopes: ScopeType.Extension,
+          }
+      ]
+    },
+    {
         basePath: 'deviceprovisioningservices/resource-manager',
         namespace: 'Microsoft.Devices',
         suffix: 'Provisioning',

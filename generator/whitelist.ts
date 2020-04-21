d4e0d8e3de952f1bd9a750f38595e44991240f43
Anthony Martin <antmarti@microsoft.com>
2020-04-21 14:37:46 -0700

    Revert "Merge pull request #962 from hunmix/onboard-logic"
    This reverts commit f6fface0f050bd70e6f87db93fd9bce74f701fa5, reversing
    changes made to 63658fcf9783e65c87a7ce7310b93091090f44cf.

--- generator/whitelist.ts
+++ generator/whitelist.ts
@@ -259,10 +259,6 @@ const whitelist: WhitelistConfig[] = [
         basePath: 'labservices/resource-manager',
         namespace: 'Microsoft.LabServices',
     },
-    {
-        basePath: 'logic/resource-manager',
-        namespace: 'Microsoft.Logic',
-    },
     { 
         basePath: 'eventgrid/resource-manager',
         namespace: 'Microsoft.EventGrid',

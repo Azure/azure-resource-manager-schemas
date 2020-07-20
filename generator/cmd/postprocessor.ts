feb90e8fc21eb80de864a08906558587d646a066
t-zhangw <codespace@72c482c53b75>
2020-07-20 07:41:54 +0000

    empty postprocessor

--- generator/cmd/postprocessor.ts
+++ generator/cmd/postprocessor.ts
@@ -1,36 +1,40 @@
-import path from 'path';
-import chalk from 'chalk';
-import { findAutogenEntriesByNamespace } from '../autogenlist';
-import { rmdirRecursive, findRecursive, executeSynchronous, readJsonFile, writeJsonFile } from '../utils';
-import * as constants from '../constants';
-import { AutogenlistConfig } from '../models';
-
-function getSchemaFileName(namespace: string, suffix: string | undefined) {
-    if (suffix === undefined) {
-        return `${namespace}.json`
-    }
-
-    return `${namespace}.${suffix}.json`;
-}
-
+import { executeSynchronous } from '../utils';
 executeSynchronous(async () => {
-    const generatedSchemasFolder = path.join(constants.generatorRoot, 'tmp');
-    const generatedSchemas = await findRecursive(generatedSchemasFolder, p => path.extname(p) === '.json');
-    try {
-        for (const generatedSchema of generatedSchemas) {
-            const output = await readJsonFile(generatedSchema);
-            console.log('Src FileName: ' + chalk.green(generatedSchema));
-            const namespace = path.basename(generatedSchema.substring(0, generatedSchema.lastIndexOf(path.extname(generatedSchema))));
-            const apiVersion = path.basename(path.resolve(`${generatedSchema}/..`));
-            const autogenlistConfig = findAutogenEntriesByNamespace(namespace);
-            const suffix = autogenlistConfig?.suffix;
-            const relativePath = `${apiVersion}/${getSchemaFileName(namespace, suffix)}`;
-            const schemaPath = path.join(constants.schemasBasePath, relativePath);
-            await writeJsonFile(schemaPath, output);
-            console.log('Dist FileName: ' + chalk.green(schemaPath));
-        }
-    }
-    finally {
-        await rmdirRecursive(generatedSchemasFolder);
-    }
 });
+
+// import path from 'path';
+// import chalk from 'chalk';
+// import { findAutogenEntriesByNamespace } from '../autogenlist';
+// import { rmdirRecursive, findRecursive, executeSynchronous, readJsonFile, writeJsonFile } from '../utils';
+// import * as constants from '../constants';
+// import { AutogenlistConfig } from '../models';
+
+// function getSchemaFileName(namespace: string, suffix: string | undefined) {
+//     if (suffix === undefined) {
+//         return `${namespace}.json`
+//     }
+
+//     return `${namespace}.${suffix}.json`;
+// }
+
+// executeSynchronous(async () => {
+    // const generatedSchemasFolder = path.join(constants.generatorRoot, 'tmp');
+    // const generatedSchemas = await findRecursive(generatedSchemasFolder, p => path.extname(p) === '.json');
+    // try {
+    //     for (const generatedSchema of generatedSchemas) {
+    //         const output = await readJsonFile(generatedSchema);
+    //         console.log('Src FileName: ' + chalk.green(generatedSchema));
+    //         const namespace = path.basename(generatedSchema.substring(0, generatedSchema.lastIndexOf(path.extname(generatedSchema))));
+    //         const apiVersion = path.basename(path.resolve(`${generatedSchema}/..`));
+    //         const autogenlistConfig = findAutogenEntriesByNamespace(namespace);
+    //         const suffix = autogenlistConfig?.suffix;
+    //         const relativePath = `${apiVersion}/${getSchemaFileName(namespace, suffix)}`;
+    //         const schemaPath = path.join(constants.schemasBasePath, relativePath);
+    //         await writeJsonFile(schemaPath, output);
+    //         console.log('Dist FileName: ' + chalk.green(schemaPath));
+    //     }
+    // }
+    // finally {
+    //     await rmdirRecursive(generatedSchemasFolder);
+    // }
+// });

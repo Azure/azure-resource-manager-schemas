7ddfcb972af355a9b0eea8c3dec745c9e9e1edb4
Jorge Cotillo <jcotillo@microsoft.com>
2021-02-22 17:32:15 -0800

    initial test

---
+++ tools/deploymentsSchemaTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace deploymentsSchemaTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

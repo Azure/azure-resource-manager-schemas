f6373149cfb81c6c2232c379410343cb6797ea75
Rucha Rangnekar <rurangne@microsoft.com>
2021-03-08 18:16:15 -0800

    Onboarding Microsoft.StoragePool for autogeneration

--- generator/autogenlist.ts
+++ generator/autogenlist.ts
@@ -510,6 +510,10 @@ const autogenlist: AutogenlistConfig[] = [
         namespace: 'Microsoft.StorageCache',
     },
     {
+        basePath: 'storagepool/resource-manager',
+        namespace: 'Microsoft.StoragePool',
+    },
+    {
         basePath: 'streamanalytics/resource-manager',
         namespace: 'Microsoft.StreamAnalytics',
     },

b1b6b1cda07a4950fc06ac6ee944c5013a32d41b
MikhailTryakhov <mitryakh@Microsoft.com>
2022-07-13 13:34:39 -0700

    added post processor

--- generator/processors/Microsoft.Network.ts
+++ generator/processors/Microsoft.Network.ts
@@ -4,5 +4,13 @@ import { SchemaPostProcessor } from '../models';
 import { replaceCyclicRef } from './helpers';
 
 export const postProcessor: SchemaPostProcessor = async (namespace, apiVersion, schema) => {
-  replaceCyclicRef(schema.definitions?.MetadataDependencies?.properties?.criteria?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.VirtualNetworkTap?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.IPConfigurationPropertiesFormat?.properties?.Subnet?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.ApplicationGatewayBackendAddressPool?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.ApplicationGatewayBackendAddressPoolPropertiesFormat?.properties?.NetworkInterfaceIPConfiguration?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.RouteFilterPropertiesFormat?.properties?.ExpressRouteCircuitPeering?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.Ipv6ExpressRouteCircuitPeeringConfig?.properties?.RouteFilter?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.PublicIPAddressPropertiesFormat?.properties?.PublicIPAddress?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.CustomIpPrefixPropertiesFormat?.properties?.CustomIpPrefix?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.CustomIpPrefixPropertiesFormat?.properties?.CustomIpPrefix?.oneOf[0]?.items);
 }

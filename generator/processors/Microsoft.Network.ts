bb8663448a8ab917733599ae29d08007a26b0e97
MikhailTryakhov <mitryakh@Microsoft.com>
2022-07-14 13:53:21 -0700

    Fixed processor

--- generator/processors/Microsoft.Network.ts
+++ generator/processors/Microsoft.Network.ts
@@ -4,13 +4,12 @@ import { SchemaPostProcessor } from '../models';
 import { replaceCyclicRef } from './helpers';
 
 export const postProcessor: SchemaPostProcessor = async (namespace, apiVersion, schema) => {
-  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.VirtualNetworkTap?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.IPConfigurationPropertiesFormat?.properties?.Subnet?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.ApplicationGatewayBackendAddressPool?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.ApplicationGatewayBackendAddressPoolPropertiesFormat?.properties?.NetworkInterfaceIPConfiguration?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.RouteFilterPropertiesFormat?.properties?.ExpressRouteCircuitPeering?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.Ipv6ExpressRouteCircuitPeeringConfig?.properties?.RouteFilter?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.PublicIPAddressPropertiesFormat?.properties?.PublicIPAddress?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.CustomIpPrefixPropertiesFormat?.properties?.CustomIpPrefix?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.CustomIpPrefixPropertiesFormat?.properties?.CustomIpPrefix?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.virtualNetworkTaps?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.publicIPAddress?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.applicationGatewayBackendAddressPools?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.ApplicationGatewayBackendAddressPoolPropertiesFormat?.properties?.backendIPConfigurations?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.RouteFilterPropertiesFormat?.properties?.peerings?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.Ipv6ExpressRouteCircuitPeeringConfig?.properties?.routeFilter?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.PublicIPAddressPropertiesFormat?.properties?.servicePublicIPAddress?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.CustomIpPrefixPropertiesFormat?.properties?.customIpPrefixParent?.oneOf[0]?.items);
 }

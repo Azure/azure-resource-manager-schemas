787d3bc26b9511be6bb4301df7553d2b6db839a1
MikhailTryakhov <mitryakh@microsoft.com>
2022-07-25 14:03:41 -0700

    Fixed few more cycles

--- generator/processors/Microsoft.Network.ts
+++ generator/processors/Microsoft.Network.ts
@@ -5,13 +5,15 @@ import { replaceCyclicRef } from './helpers';
 
 export const postProcessor: SchemaPostProcessor = async (namespace, apiVersion, schema) => {
   replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.virtualNetworkTaps?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfiguration?.properties?.properties?.oneOf[0]?.properties?.publicIPAddress?.properties?.properties?.oneOf[0]?.properties?.ipConfiguration?.properties?.properties?.oneOf[0]?.properties?.subnet?.properties?.properties?.properties?.networkSecurityGroup.properties?.properties?.oneOf[0]?.properties?.networkInterfaces.properties?.properties?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfiguration?.properties?.properties?.oneOf[0]?.properties?.publicIPAddress?.oneOf[0].properties?.properties?.oneOf[0]?.properties?.ipConfiguration?.oneOf[0].properties?.properties?.oneOf[0]?.properties?.subnet?.oneOf[0].properties?.properties?.oneOf[0].properties?.networkSecurityGroup.oneOf[0].properties?.properties?.oneOf[0]?.properties?.networkInterfaces.oneOf[0].properties?.properties?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.ipConfiguration?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.applictionGatewayBackendAddressPools?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.ApplicationGatewayBackendAddressPoolPropertiesFormat?.properties?.backendIPConfigurations?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.RouteFilterPropertiesFormat?.properties?.peerings?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.Ipv6ExpressRouteCircuitPeeringConfig?.properties?.routeFilter?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.PublicIPAddress?.properties?.properties?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.PublicIPAddressPropertiesFormat?.properties?.linkedPublicIPAddress?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.CustomIpPrefix?.properties?.properties?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.CustomIpPrefixPropertiesFormat?.properties?.customIpPrefixParent?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.ContainerNetworkInterfaceConfigurationPropertiesFormat?.properties?.containerNetworkInterfaces?.oneOf[0]?.items);
 }

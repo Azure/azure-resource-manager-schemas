7fc830e1048c33f46d93ad338ebe60ea2651f5df
MikhailTryakhov <mitryakh@Microsoft.com>
2022-08-04 12:14:49 -0700

    Fixed one more cycle fix

--- generator/processors/Microsoft.Network.ts
+++ generator/processors/Microsoft.Network.ts
@@ -5,7 +5,7 @@ import { replaceCyclicRef } from './helpers';
 
 export const postProcessor: SchemaPostProcessor = async (namespace, apiVersion, schema) => {
   replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.virtualNetworkTaps?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfiguration?.properties?.properties?.oneOf[0]?.properties?.publicIPAddress?.oneOf[0].properties?.properties?.oneOf[0]?.properties?.ipConfiguration?.oneOf[0].properties?.properties?.oneOf[0]?.properties?.subnet?.oneOf[0].properties?.properties?.oneOf[0].properties?.networkSecurityGroup.oneOf[0].properties?.properties?.oneOf[0]?.properties?.networkInterfaces.oneOf[0].items);
+  replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfiguration?.properties?.properties?.oneOf[0]?.properties?.publicIPAddress?.oneOf[0]?.properties?.properties?.oneOf[0]?.properties?.ipConfiguration?.oneOf[0]?.properties?.properties?.oneOf[0]?.properties?.subnet?.oneOf[0]?.properties?.properties?.oneOf[0]?.properties?.networkSecurityGroup?.oneOf[0]?.properties?.properties?.oneOf[0]?.properties?.networkInterfaces?.oneOf[0].items);
   replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.ipConfiguration?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.NetworkInterfaceIPConfigurationPropertiesFormat?.properties?.applictionGatewayBackendAddressPools?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.ApplicationGatewayBackendAddressPoolPropertiesFormat?.properties?.backendIPConfigurations?.oneOf[0]?.items);

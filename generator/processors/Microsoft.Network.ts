007c1083624d305f0b9b1b9c2ff7356c248ad80a
MikhailTryakhov <mitryakh@microsoft.com>
2022-08-03 18:46:24 -0700

    Fixed processor params

--- generator/processors/Microsoft.Network.ts
+++ generator/processors/Microsoft.Network.ts
@@ -11,8 +11,8 @@ export const postProcessor: SchemaPostProcessor = async (namespace, apiVersion, 
   replaceCyclicRef(schema.definitions?.ApplicationGatewayBackendAddressPoolPropertiesFormat?.properties?.backendIPConfigurations?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.RouteFilterPropertiesFormat?.properties?.peerings?.oneOf[0]?.items);
   replaceCyclicRef(schema.definitions?.Ipv6ExpressRouteCircuitPeeringConfig?.properties?.routeFilter?.oneOf[0]?.items);
-  replaceCyclicRef(schema.definitions?.PublicIPAddressPropertiesFormat?.properties?.oneOf[0]?.properties?.linkedPublicIPAddress?.oneOf[0]?);
-  replaceCyclicRef(schema.definitions?.PublicIPAddressPropertiesFormat?.properties?.oneOf[0]?.properties?.servicePublicIPAddress?.oneOf[0]?);
-  replaceCyclicRef(schema.definitions?.CustomIpPrefix?.properties?.properties?.oneOf[0]?.properties?.customIpPrefixParent?.oneOf[0]?);
+  replaceCyclicRef(schema.definitions?.PublicIPAddressPropertiesFormat?.properties?.oneOf[0]?.properties?.linkedPublicIPAddress?.oneOf[0]);
+  replaceCyclicRef(schema.definitions?.PublicIPAddressPropertiesFormat?.properties?.oneOf[0]?.properties?.servicePublicIPAddress?.oneOf[0]);
+  replaceCyclicRef(schema.definitions?.CustomIpPrefix?.properties?.properties?.oneOf[0]?.properties?.customIpPrefixParent?.oneOf[0]);
   replaceCyclicRef(schema.definitions?.ContainerNetworkInterfaceConfigurationPropertiesFormat?.properties?.containerNetworkInterfaces?.oneOf[0]?.items);
 }

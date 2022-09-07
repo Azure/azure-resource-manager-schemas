5bd40c5360b12d16f470af3fbf9b67d66f95bb48
Jorge Cotillo <jcotillo@microsoft.com>
2022-09-07 11:32:55 -0700

    remove cyclic reference

--- generator/processors/Microsoft.SecurityInsights.ts
+++ generator/processors/Microsoft.SecurityInsights.ts
@@ -5,4 +5,5 @@ import { replaceCyclicRef } from './helpers';
 
 export const postProcessor: SchemaPostProcessor = async (namespace, apiVersion, schema) => {
   replaceCyclicRef(schema.definitions?.MetadataDependencies?.properties?.criteria?.oneOf[0]?.items);
+  replaceCyclicRef(schema.definitions?.AutomationRuleCondition?.oneOf[0]);
 }

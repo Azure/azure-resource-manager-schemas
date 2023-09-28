7a36d2b415bbb6d8a6cf14907981adb47697fa36
Gokul Premraj <gopremra@microsoft.com>
2023-09-28 18:52:45 -0400

    Fix linter issues

--- generator/processors/Microsoft.ServiceFabric.ts
+++ generator/processors/Microsoft.ServiceFabric.ts
@@ -4,7 +4,7 @@ import { SchemaPostProcessor } from '../models';
 
 export const postProcessor: SchemaPostProcessor = async (namespace, apiVersion, schema) => {
 
-    var IpTag = schema.definitions?.IpConfigurationPublicIPAddressConfiguration?.properties?.ipTags?.oneOf[0].items?.$ref;
+    let IpTag = schema.definitions?.IpConfigurationPublicIPAddressConfiguration?.properties?.ipTags?.oneOf[0].items?.$ref;
     if (IpTag != null){
         IpTag = "#/definitions/IPTag";
     }

ccd8f4922a5130661279c420429a7abffce5c724
Jorge Cotillo <jcotillo@microsoft.com>
2023-12-21 15:00:56 -0800

    added missing files, fixed build warning

--- generator/processors/Microsoft.ServiceFabric.ts
+++ generator/processors/Microsoft.ServiceFabric.ts
@@ -4,7 +4,7 @@ import { SchemaPostProcessor } from '../models';
 
 export const postProcessor: SchemaPostProcessor = async (namespace, apiVersion, schema) => {
 
-    let IpTag = schema.definitions?.IpConfigurationPublicIPAddressConfiguration?.properties?.ipTags?.oneOf[0].items;
+    const IpTag = schema.definitions?.IpConfigurationPublicIPAddressConfiguration?.properties?.ipTags?.oneOf[0].items;
     if (IpTag && IpTag['$ref']) {
         IpTag['$ref'] = '#/definitions/IPTag';
     }
